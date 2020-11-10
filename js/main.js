9450684aa3929e9cbe4df03fcf51175feb8b9e53
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-11-10 14:25:00 +0100

    v-for added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const app = new Vue ({
+    el:'#app',
+
+    data: {
+        objectives: [
+            { obj: 'Fare la spesa' },
+            { obj: 'Chiamare nonna' },
+            { obj: 'Controllare offerte Amazon' },
+
+        ]
+    }
+
+});
